bd908775204d2809b214be7a74724154b1e204d7
Rudransh Pratap Singh <hackerboyrps@gmail.com>
2024-07-07 13:44:47 +0530

    Added Functionality

--- bot.py
+++ bot.py
@@ -1,30 +1,50 @@
 import time
 import pyautogui as pyg
-print("It's Gonna Start Now... Move your text cursor focus to the")
-print("Required Text Field")
 
-time.sleep(30)
+print("Press 1 for Default Spam\nPress 2 for Bee Spam")
 
-for i in range(100):
-    pyg.press("Y")
-    pyg.press("u")
-    pyg.press("o")
-    pyg.press("enter")
-    pyg.press("a")
-    pyg.press("r")
-    pyg.press("e")
-    pyg.press("enter")
-    pyg.press("s")
-    pyg.press("p")
-    pyg.press("a")
-    pyg.press("m")
-    pyg.press("m")
-    pyg.press("e")
-    pyg.press("d")
-    pyg.press("enter")
-    pyg.press("b")
-    pyg.press("y")
-    pyg.press("enter")
-    pyg.press("R")
-    pyg.press("P")
-    pyg.press("S")
+in = input("-->")
+
+if (in == 1) :
+    print("It's Gonna Start Now... Move your text cursor focus to the")
+    print("Required Text Field")
+
+    time.sleep(30)
+
+    for i in range(100):
+        pyg.press("Y")
+        pyg.press("u")
+        pyg.press("o")
+        pyg.press("enter")
+        pyg.press("a")
+        pyg.press("r")
+        pyg.press("e")
+        pyg.press("enter")
+        pyg.press("s")
+        pyg.press("p")
+        pyg.press("a")
+        pyg.press("m")
+        pyg.press("m")
+        pyg.press("e")
+        pyg.press("d")
+        pyg.press("enter")
+        pyg.press("b")
+        pyg.press("y")
+        pyg.press("enter")
+        pyg.press("R")
+        pyg.press("P")
+        pyg.press("S")
+
+elif (in == 2):
+    print("It's Gonna Start Now... Move your text cursor focus to the")
+    print("Required Text Field")
+    time.sleep(30)f = open("bee.txt", "r")
+    for word in f:
+        pyautogui.typewrite(word)
+        pyautogui.press("enter")
+
+
+
+
+
+
